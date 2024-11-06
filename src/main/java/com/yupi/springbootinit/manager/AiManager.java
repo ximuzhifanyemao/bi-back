b1098e67d2b14bb6e0cfef69026a502590919ce4
liunian_ximu <1323871132@qq.com>
2024-11-06 22:31:45 +0800

    更换大模型

--- src/main/java/com/yupi/springbootinit/manager/AiManager.java
+++ src/main/java/com/yupi/springbootinit/manager/AiManager.java
@@ -70,7 +70,6 @@ public class AiManager {
                 // 指定请求版本，默认使用最新2.0版本
                 .apiVersion(SparkApiVersion.V3_5)
                 .build();
-
         // 同步调用
         SparkSyncChatResponse chatResponse = sparkClient.chatSync(sparkRequest);
         String responseContent = chatResponse.getContent();
